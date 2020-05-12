f77a75b1a8bbdef6774aa199508f9bd4184e1249
Awais Hassan <55058852+AWAIS97@users.noreply.github.com>
2020-05-12 02:08:23 -0700

    Updates

---
+++ precache-manifest.1d381cb231c3baef3fa2156c0f73d561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff7b754d48a0772bec5b9040e8cdfaf",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "e48599fea7f12fd0104a",
+    "url": "/Covid-19/static/css/main.450c22df.chunk.css"
+  },
+  {
+    "revision": "dbdc1d19321bd165024c",
+    "url": "/Covid-19/static/js/2.23ddaa74.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19/static/js/2.23ddaa74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e48599fea7f12fd0104a",
+    "url": "/Covid-19/static/js/main.8ce69e47.chunk.js"
+  },
+  {
+    "revision": "57219de4493609df23fc",
+    "url": "/Covid-19/static/js/runtime-main.f3f4ba94.js"
+  },
+  {
+    "revision": "83dc46cba8d4876a295c48f2b1b4ae81",
+    "url": "/Covid-19/static/media/components.83dc46cb.png"
+  }
+]);
